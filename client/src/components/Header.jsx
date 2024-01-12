18a220cf45a64af7f27648d339dc16ab50cf50bc
Rince Mathew <rincemathewmanattu@gmail.com>
2024-01-12 20:02:44 +0530

    started backend

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,8 +8,8 @@ function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to='/'>
       <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-        <span className="text-slate-500">real</span>
-        <span className="text-slate-700">estate</span>
+        <span className="text-slate-500">Property</span>
+        <span className="text-slate-700">Lookup</span>
       </h1>
       </Link>
       <form className="bg-slate-100 p-3 rounded-lg flex items-center" action="">
