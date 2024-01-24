16dc770dc9d443871bec0426785500cc40c56572
Rince Mathew <rincemathewmanattu@gmail.com>
2024-01-24 13:43:03 +0530

    created custom error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,16 @@
 import User from '../model/user.model.js'
 import bcryptjs from 'bcryptjs';
 
-const signUp = async(req,res)=> {
+const signUp = async(req,res,next)=> {
     const {username, email, password} = req.body;
+    console.log(username,email,password)
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({username, email, password:hashedPassword});
     try{
      await newUser.save();
      res.status(201).json('User created successfully')
     } catch(error) {
-        res.status(500).json(error.message)
+        next(error)
     }
 };
 
--- api/server.js
+++ api/server.js
@@ -1,26 +1,36 @@
 import express from "express";
 import connectToMongoDB from "./helpers/mongoConnect.js";
-import userRouter from './routes/user.route.js'
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 const app = express();
 
-
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 import vhost from "vhost";
 
+app.use(express.json());
 
-app.use(express.json())
-
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // const adminRouter = require('./routers/adminRoute')
 
-connectToMongoDB()
+connectToMongoDB();
 //used vhost to create subdomains
-app.use(vhost('localhost', userRouter));
+app.use(vhost("localhost", userRouter));
+app.use(vhost("localhost", authRouter));
 // app.use(vhost(`admin.localhost`, adminRouter));
 
-app.listen(process.env.PORT,()=>{
-    console.log('server started')
-})
+app.listen(process.env.PORT, () => {
+  console.log("server started");
+});
+
+app.use((err, req, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return resizeBy.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
