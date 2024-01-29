9ee7d49a132be04e018debbc97433b2d3cb5fb1d
Rince Mathew <rincemathewmanattu@gmail.com>
2024-01-29 20:38:42 +0530

    oauth user save

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,10 +3,12 @@ const router = express.Router();
 import dotenv from "dotenv";
 dotenv.config();
 
-import { signUp,signIn } from "../controllers/auth.controller.js";
+import { signUp,signIn, google } from "../controllers/auth.controller.js";
 
 router.post(`${process.env.COMMON_URL}/signup`,signUp);
 
 router.post(`${process.env.COMMON_URL}/signin`,signIn)
+
+router.post(`${process.env.COMMON_URL}/google`,google)
 
 export default router;
