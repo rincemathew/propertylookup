e33a9b0ac2dbaee7b66717b5aa8640f7122b62e6
Rince Mathew <rincemathewmanattu@gmail.com>
2025-08-18 19:06:25 +0530

    bycript error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,20 @@
 import User from "../models/user.model.js";
-import bcrypt from 'bcrypt';
+import bcryptjs from 'bcryptjs';
 
 export const signup = async(req, res) => {
   const { username,email, password } = req.body; 
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcryptjs.hashSync(password, 10);
   const newUser = new User({
     username,
     email,
     password:hashedPassword
   });
-  await newUser.save();
-  res.status(201).json({ message: "User created successfully" });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+    
+  } catch (error) {
+    res.status(500).json({ message: "Error creating user", error: error.message });
+  }
 
 }
